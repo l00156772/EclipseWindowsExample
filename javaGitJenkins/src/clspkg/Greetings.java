3c89c5e70baec46c73fac237046f0ad97b92581d
SMT <l00156772@student.lyit.ie>
2020-02-18 00:16:15 -0500

    Added 2nd sys out line

--- javaGitJenkins/src/clspkg/Greetings.java
+++ javaGitJenkins/src/clspkg/Greetings.java
@@ -15,6 +15,7 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greetings LYIT Sowmya");
+		System.out.println("Sowmya Testing 2nd commit");
 	}
 
 }
