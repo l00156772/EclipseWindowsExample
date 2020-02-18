c19903956796cd0fe10309e486eca3b10a536621
SMT <l00156772@student.lyit.ie>
2020-02-18 01:11:49 -0500

    Sowmya Testing 4th commit with Jenkins

--- javaGitJenkins/src/clspkg/Greetings.java
+++ javaGitJenkins/src/clspkg/Greetings.java
@@ -17,6 +17,7 @@ public class Greetings {
 		System.out.println("Greetings LYIT Sowmya");
 		System.out.println("Sowmya Testing 2nd commit");
 		System.out.println("Sowmya Testing 3rd commit");
+		System.out.println("Sowmya Testing 4rd commit with Jenkins");
 	}
 
 }
